20d1fbb87004d7cde6e1b28ffd89e6ab23e9632b
Cam Kemshal-Bell <cam@iterated.tech>
2025-02-26 01:31:17 +1100

    fix: revert commit SHA-93c655f

--- src/Support/Screen.php
+++ src/Support/Screen.php
@@ -90,14 +90,8 @@ class Screen
 
     public function write(string $content): static
     {
-        // Convert raw backspace (BS or DEL) into an ANSI “cursor backward”
-        // so that your existing handleAnsiCode('\e[D') picks it up.
-        // Also convert carriage returns to cursor-col=0:
-        $content = str_replace(
-            search: ["\x08", "\x7f", "\r"],
-            replace: ["\e[D", "\e[D", "\e[G"],
-            subject: $content
-        );
+        // Carriage returns get replaced with a code to move to column 0.
+        $content = str_replace("\r", "\e[G", $content);
 
         // Split the line by ANSI codes. Each item in the resulting array
         // will be a set of printable characters or an ANSI code.
